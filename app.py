b5c6d588bc71f7ea3678561174090a9206b46da5
Osama <osamalfaify@gmail.com>
2024-04-04 23:34:49 +0300

    This Streamlit-based web application leverages advanced NLP and AI to provide insightful analysis and responses based on marketing-related articles.

---
+++ app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+load_dotenv()
+
+def get_vectorstore_from_url(url):
+    loader = WebBaseLoader(url)
+    documnet = loader.load()
+    
+    #split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    documnet_chunks = text_splitter.split_documents(documnet)
+    
+    # create vectore store for each chunk
+    vector_store = Chroma.from_documents(documnet_chunks,OpenAIEmbeddings())
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+    retriever = vector_store.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the article you added, can you tell me more about it?"),
+    ])
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    return retriever_chain
+
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the following questions based on the article added:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+# function to get response
+def get_response(user_query):
+    # create conversation chain
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversational_rag_chain = get_conversational_rag_chain(retriever_chain)
+    respons = conversational_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_query,
+    })
+    return respons['answer']
+
+# app configuration
+st.set_page_config(page_title="Marketing AI Advisor", page_icon="📈")
+st.title("Marketing AI Advisor 🎯")
+
+
+# app sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Article URL")
+if website_url is None or website_url == "":
+    st.info("Please enter a valid article URL to be analyzed.")
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello! I'm your Marketing AI Advisor. How can I help you today? 🤖"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)
+        
+    # user input
+    user_query = st.chat_input("Ask me anything about the article that you added...")
+    if user_query is not None and user_query != "":
+        respons = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=respons))
+        
+    # conversation history
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
